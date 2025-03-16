f95b78cb8f0e82b31f8282cb2b0dd98c867f0679
balazsreka11 <150653786+balazsreka11@users.noreply.github.com>
2025-03-16 13:56:43 +0100

    Add files via upload

---
+++ lidl_social_media_scraping.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 16 13:46:10 2025
+
+@author: Asus
+"""
+
+import requests
+
+# API kulcs és CSE ID
+api_key = "AIzaSyBeU-505OOFSMgqEwNVz0vCfCFuTk8XPFE"  # Cseréld le a saját API kulcsodra
+cx = "f2c1ef21db05c49e1"  # Cseréld le a saját CSE ID-dra
+
+# Keresési kulcsszavak
+query = 'LIDL ("AI" OR "Machine Learning" OR "Data Science") site:instagram.com OR site:twitter.com OR site:facebook.com'  # LIDL szó és az egyik kívánt kifejezés
+
+# URL és kérés paraméterek
+url = "https://www.googleapis.com/customsearch/v1"
+
+params = {
+    'key': api_key,  # API kulcs
+    'cx': cx,        # CSE ID
+    'q': query,      # Keresési lekérdezés
+}
+
+# HTTP GET kérés
+response = requests.get(url, params=params)
+
+lista = []
+
+# Ellenőrizzük, hogy sikeres volt-e a kérés
+if response.status_code == 200:
+    # JSON válasz feldolgozása
+    search_results = response.json()
+    items = search_results.get('items', [])
+
+    if items:
+        for item in items:
+            title = item['title']
+            link = item['link']
+            snippet = item['snippet']
+            lista.append(f"Title: {title}\nLink: {link}\nSnippet: {snippet}\n")
+            print(f"Title: {title}\nLink: {link}\nSnippet: {snippet}\n")
+    else:
+        print("Nem találtunk találatokat.")
+else:
+    print("Hiba történt a kérés során.")
