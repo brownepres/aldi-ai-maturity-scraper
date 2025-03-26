f3c044ec14e7094869b332489d1fe6a33f6d1f41
brownepres <barnabas.epres@gmail.com>
2025-03-26 12:06:23 +0100

    added and modified gitignore

--- app/app.py
+++ app/app.py
@@ -5,6 +5,8 @@ def main():
     print("calling news scraper...")
     result = scrapeNews()
     print(result)
+    #todo: call all other functions
+    #shape data into csv
 
 
 if __name__ == '__main__':
--- app/src/scrape_news.py
+++ app/src/scrape_news.py
@@ -1,2 +1,10 @@
+import requests
 def scrapeNews():
-    return "news have been scraped"
+    url = ('https://newsapi.org/v2/everything?'
+       'q=Apple&AI'
+       'from=2025-03-10&'
+       'sortBy=popularity&'
+       'apiKey=237d140128514a4bb46272ece0d0bbff')
+
+    response = requests.get(url).json()
+    return response
