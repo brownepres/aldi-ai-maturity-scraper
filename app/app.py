d3ab46fa6c7c2f0033c85a36c34cce55712c47c5
brownepres <barnabas.epres@gmail.com>
2025-05-13 15:06:24 +0200

    changes in running algo

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
 from src.news2 import ScrapeNews
 from src.social_media import getSocialMedia
 from src.oldalak_diagram import pages
+from src.penzugyiadatok import get_financial_data
+from src.create_visuals import makeVisuals
 import pandas as pd
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 import os
 import sys
+import numpy as np
 
 nltk.download('vader_lexicon')  # Only once
 sia = SentimentIntensityAnalyzer()
@@ -25,16 +28,17 @@ companies = pd.read_excel(file_path, sheet_name='companies-english')
 key_words = pd.read_excel(file_path, sheet_name='keywords-english')
 key_words = key_words['keywords'].to_list()
 companies_list = companies['company_name'].to_list()
-print(companies_list)
 
 list_for_df = []
 
 def main():
     for i, company in enumerate(companies_list):
         print(f'Analyzing for: {company}')
-        if i == 1:
+        if i == 100:
             break
         list_item = [i, company]
+
+        #scraping the news
         try:
             news = ScrapeNews(company)
             
@@ -58,22 +62,51 @@ def main():
             list_item.append(keywords_count)
         except:
             print("The news could not be collected due to an error")
-            list_item.append(None)
-            list_item.append(None)
-            list_item.append(None)
+            list_item.append(0)
+            list_item.append(0)
+            list_item.append(0)
         
+        #scraping social media
         try:
-            social_media = getSocialMedia(company)
-            print(social_media)    
+            social_media = getSocialMedia(company) 
             list_item.append(social_media)
         except Exception as e:
             print(f"Social media data could not be collected due to an error. {e}")
-            list_item.append(None)
+            list_item.append(0)
 
+        try:
+            total_revenue, gross_profit, profit_rate = get_financial_data(company)
+            for i in total_revenue:
+                list_item.append(i)
+            for i in gross_profit:
+                list_item.append(i)
+            for i in profit_rate:
+                list_item.append(i)
+                
+        except Exception as e:
+            for i in range(4):
+                list_item.append(0)
+            for i in range(4):
+                list_item.append(0)
+            for i in range(4):
+                list_item.append(0)
+            
+            print(f"Collecting financial data has produced an error: {e}")
         list_for_df.append(list_item)
         print(i)
         
-    output = pd.DataFrame(list_for_df, columns=['Id', 'Company', 'Number of news', 'Average news sentiment', 'Number of AI key words', 'Average social media sentiment'])    
+    output = pd.DataFrame(list_for_df, columns=['Id', 'Company', 'Number of news', 'Average news sentiment', 'Number of AI key words', 'Average social media sentiment',
+                                                 '2021total_revenue', '2021gross_proft', "2021profit_rate", 
+                                                 '2022total_revenue', '2022gross_proft', "2022profit_rate", 
+                                                 '2023total_revenue', '2023gross_proft', "2023profit_rate", 
+                                                 '2024total_revenue', '2024gross_proft', "2024profit_rate"])    
+    
+    output['AI buzz score'] = (output['Number of AI key words'] + 1) * output[['Average news sentiment', 'Average social media sentiment']].mean(axis=1) * np.log10(output['Number of news'] + 2) * 100
+    
+    #create visuals
+    print("Creating report with graphs...")
+    output.to_csv('output.csv')
+    result = makeVisuals(output)
     output.to_csv('output.csv')
 
     #scrape the desired pages
